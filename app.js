684c16df2b57a43f360b931c99f1f36b6866cd80
tiagojard <tiago.jardcarvalho@gmail.com>
2019-02-27 20:26:35 -0300

    add

---
+++ app.js
@@ -0,0 +1,46 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var clients = {}; 
+
+app.get('/', function(req, res){
+     // Website you wish to allow to connect
+     res.setHeader('Access-Control-Allow-Origin', '*');
+
+     // Request methods you wish to allow
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+     // Request headers you wish to allow
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+     // Set to true if you need the website to include cookies in the requests sent
+     // to the API (e.g. in case you use sessions)
+     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.send('server is running');
+});
+
+io.on("connection", function (client) {  
+    client.on("join", function(name){
+    	console.log("Joined: " + name);
+        clients[client.id] = name;
+        client.emit("update", "You have connected to the server.");
+        client.broadcast.emit("update", name + " has joined the server.")
+    });
+
+    client.on("send", function(msg){
+    	console.log("Message: " + msg);
+        client.broadcast.emit("chat", clients[client.id], msg);
+    });
+
+    client.on("disconnect", function(){
+    	console.log("Disconnect");
+        io.emit("update", clients[client.id] + " has left the server.");
+        delete clients[client.id];
+    });
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
